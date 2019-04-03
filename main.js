556db5e57223289a42a28c22078dbe8287f4de0f
Amber Jones <amberlovescats14@gmail.com>
2019-04-03 12:46:19 -0500

    full name is coming up undefined

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 // dodgeBall Javascript
+
+
+class Person {
+  static action() {
+    return 'Signed Up'
+  }
+  static actionSentence() {
+    return `This person has ${this.action}`
+  }
+  constructor(first, last){
+    this.first = first
+    this.last = last
+  }
+  fullName() {
+    
+    // const fullName = `${this.first} ${this.last}`
+    document.getElementById('players').innerHTML = `${this.first} ${this.last}`
+  
+  }
+
+  setFirstName(first){
+    this.first = first
+  }
+  setLastName(last){
+    this.last = last
+  }
+  
+
+}
+let p = new Person
+const person1 = new Person('Amber', 'Jones')
+document.getElementById('button').addEventListener('click', person1.fullName)
